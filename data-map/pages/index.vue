d0a9dbfd95f76a5e71d0d07f7399a40a5b4493c4
sheng zeng <bahtmannn@gmail.com>
2019-10-02 01:12:49 +1000

    Added extra details

--- data-map/pages/index.vue
+++ data-map/pages/index.vue
@@ -1,7 +1,12 @@
 <template>
   <v-layout>
     <v-flex>
-      <div id="map-wrap" style="height: 100vh; width: 130vh">
+      <div>
+          <h1>From {{this.fromCity}} to {{toCity}} cost ${{toCost}}, ${{returnCost}} return</h1> 
+          <p>To- {{toPlaneModel}} plane model and {{toEngineModel}} engine model</p>
+          <p>Return-  {{fromPlaneModel}} plane model and {{fromEngineModel}} engine mode</p>
+      </div>
+      <div id="map-wrap" style="height: 100vh; width: 140vh">
  <no-ssr>
    <l-map :zoom=5 :center="[-29, 133.7751]">
      <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
@@ -26,38 +31,34 @@
      <l-circle-marker  :radius="5" :color="markerColour" :lat-lng="markers.cairns"></l-circle-marker>
      <l-circle-marker @mouseover="mtAugusta()" @mouseleave="mtAugustaLeave()" :radius="5" :color="markerColour" :lat-lng="markers.ptAugusta"></l-circle-marker>
 
-     <l-polyline :weight="3" :lat-lngs="polyline.sydMelb" :color="sydMelb"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.sydBris" :color="sydBris"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.sydCanb" :color="sydCanb"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.sydNew" :color="sydNew"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.sydBroke" :color="sydBroke"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.melbCanb" :color="melbCanb"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.melbAde" :color="melbAde"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.melbHob" :color="melbHob"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.melbBen" :color="melbBen"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.melbLaun" :color="melbLaun"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.adeBroke" :color="adeBroke"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.adePerth" :color="adePerth"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.adeDar" :color="adeDar"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.darAlice" :color="darAlice"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.perthAlb" :color="perthAlb"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.perthKal" :color="perthKal"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.perthBro" :color="perthBro"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.launHob" :color="launHob"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.brisMt" :color="brisMt"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.brisRock" :color="brisRock"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.brisCair" :color="brisCair"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.brisDar" :color="brisDar"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.mtDar" :color="mtDar"></l-polyline>
-     <l-polyline :weight="3" :lat-lngs="polyline.adePt" :color="adePt"></l-polyline>
-
+     <l-polyline @mousedown="sydMelbMethod()" :weight="3" :lat-lngs="polyline.sydMelb" :color="sydMelb"></l-polyline>
+     <l-polyline @mousedown="sydBrisMethod()" :weight="3" :lat-lngs="polyline.sydBris" :color="sydBris"></l-polyline>
+     <l-polyline @mousedown="sydCanbMethod()" :weight="3" :lat-lngs="polyline.sydCanb" :color="sydCanb"></l-polyline>
+     <l-polyline @mousedown="sydNewMethod()" :weight="3" :lat-lngs="polyline.sydNew" :color="sydNew"></l-polyline>
+     <l-polyline @mousedown="sydBrokeMethod()" :weight="3" :lat-lngs="polyline.sydBroke" :color="sydBroke"></l-polyline>
+     <l-polyline @mousedown="melbCanbMethod()" :weight="3" :lat-lngs="polyline.melbCanb" :color="melbCanb"></l-polyline>
+     <l-polyline @mousedown="melbAdeMethod()" :weight="3" :lat-lngs="polyline.melbAde" :color="melbAde"></l-polyline>
+     <l-polyline @mousedown="melbHobMethod()" :weight="3" :lat-lngs="polyline.melbHob" :color="melbHob"></l-polyline>
+     <l-polyline @mousedown="melbBenMethod()" :weight="3" :lat-lngs="polyline.melbBen" :color="melbBen"></l-polyline>
+     <l-polyline @mousedown="melbLaunMethod()" :weight="3" :lat-lngs="polyline.melbLaun" :color="melbLaun"></l-polyline>
+     <l-polyline @mousedown="adeBrokeMethod()" :weight="3" :lat-lngs="polyline.adeBroke" :color="adeBroke"></l-polyline>
+     <l-polyline @mousedown="adePerthMethod()" :weight="3" :lat-lngs="polyline.adePerth" :color="adePerth"></l-polyline>
+     <l-polyline @mousedown="adeDarMethod()" :weight="3" :lat-lngs="polyline.adeDar" :color="adeDar"></l-polyline>
+     <l-polyline @mousedown="darAliceMethod()" :weight="3" :lat-lngs="polyline.darAlice" :color="darAlice"></l-polyline>
+     <l-polyline @mousedown="perthAlbMethod()" :weight="3" :lat-lngs="polyline.perthAlb" :color="perthAlb"></l-polyline>
+     <l-polyline @mousedown="perthKalMethod()" :weight="3" :lat-lngs="polyline.perthKal" :color="perthKal"></l-polyline>
+     <l-polyline @mousedown="perthBroMethod()" :weight="3" :lat-lngs="polyline.perthBro" :color="perthBro"></l-polyline>
+     <l-polyline @mousedown="launHobMethod()" :weight="3" :lat-lngs="polyline.launHob" :color="launHob"></l-polyline>
+     <l-polyline @mousedown="brisMtMethod()" :weight="3" :lat-lngs="polyline.brisMt" :color="brisMt"></l-polyline>
+     <l-polyline @mousedown="brisRockMethod()" :weight="3" :lat-lngs="polyline.brisRock" :color="brisRock"></l-polyline>
+     <l-polyline @mousedown="brisCairMethod()" :weight="3" :lat-lngs="polyline.brisCair" :color="brisCair"></l-polyline>
+     <l-polyline @mousedown="brisDarMethod()" :weight="3" :lat-lngs="polyline.brisDar" :color="brisDar"></l-polyline>
+     <l-polyline @mousedown="mtDarMethod()" :weight="3" :lat-lngs="polyline.mtDar" :color="mtDar"></l-polyline>
+     <l-polyline @mousedown="adePtMethod()" :weight="3" :lat-lngs="polyline.adePt" :color="adePt"></l-polyline>
    </l-map>
  </no-ssr>
 </div>
     </v-flex>
-    <svg width="100" height="100">
-  <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
-</svg>
   </v-layout>
 </template>
 
@@ -116,7 +117,15 @@ export default {
       brisRock: '#EF476F',
       brisCair: '#EF476F',
       mtDar: '#FFD166',
-      adePt: '#FFD166'
+      adePt: '#FFD166',
+      fromCity: "",
+      toCity: "",
+      toCost: "",
+      returnCost: "",
+      toPlaneModel: "",
+      toEngineModel: "",
+      fromPlaneModel: "",
+      fromEngineModel: ""
     };
   },
   computed: {
@@ -227,6 +236,246 @@ export default {
      },
      mtAugustaLeave() {
        this.adePt = this.classB
+     },
+     sydMelbMethod() {
+      this.fromCity = "Sydney"
+      this.toCity= "Melbourne"
+      this.toCost= "180.00"
+      this.returnCost= "180.00"
+      this.toPlaneModel= "A330-203"
+      this.toEngineModel= "CF6-80E142"
+      this.fromPlaneModel= "A330-243"
+      this.fromEngineModel= "772B-60"
+     },
+     sydBrisMethod() {
+      this.fromCity = "Sydney"
+      this.toCity= "Brisbane"
+      this.toCost= "170.00"
+      this.returnCost= "170.00"
+      this.toPlaneModel= "A330-202"
+      this.toEngineModel= "CF6-80E142"
+      this.fromPlaneModel= "A330-203"
+      this.fromEngineModel= "CF6-80E142"
+     },
+     sydCanbMethod() {
+      this.fromCity = "Sydney"
+      this.toCity= "Canberra"
+      this.toCost= "120.00"
+      this.returnCost= "120.00"
+      this.toPlaneModel= "B737-3B7"
+      this.toEngineModel= "CFM56-3B1"
+      this.fromPlaneModel= "B737-476"
+      this.fromEngineModel= "CFM-56-3"
+     },
+     sydNewMethod() {
+      this.fromCity = "Sydney"
+      this.toCity= "Newcastle"
+      this.toCost= "90.00"
+      this.returnCost= "90.00"
+      this.toPlaneModel= "A320-232"
+      this.toEngineModel= "V2527-5A"
+      this.fromPlaneModel= "A320-232"
+      this.fromEngineModel= "V2527-5A"
+     },
+     sydBrokeMethod() {
+      this.fromCity = "Sydney"
+      this.toCity= "Broken Hill"
+      this.toCost= "130.00"
+      this.returnCost= "130.00"
+      this.toPlaneModel= "A320-232"
+      this.toEngineModel= "V2527-5A"
+      this.fromPlaneModel= "A320-232" 
+      this.fromEngineModel= "V2527-5A"
+     },
+     melbCanbMethod() {
+      this.fromCity = "Melbourne"
+      this.toCity= "Canberra"
+      this.toCost= "140.00"
+      this.returnCost= "140.00"
+      this.toPlaneModel= "A320-232"
+      this.toEngineModel= "V2527-5A"
+      this.fromPlaneModel= "A320-232" 
+      this.fromEngineModel= "V2527-5A"
+     },
+     melbAdeMethod() {
+      this.fromCity = "Melbourne"
+      this.toCity= "Adelaide"
+      this.toCost= "175.00"
+      this.returnCost= "175.00"
+      this.toPlaneModel= "B737-3B7"
+      this.toEngineModel= "CFM56-3B1"
+      this.fromPlaneModel= "B737-3B7"
+      this.fromEngineModel= "CFM56-3B1"
+     },
+      melbHobMethod() {
+      this.fromCity = "Melbourne"
+      this.toCity= "Hobart"
+      this.toCost= "130.00"
+      this.returnCost= "130.00"
+      this.toPlaneModel= "A320-232"
+      this.toEngineModel= "V2527-5A"
+      this.fromPlaneModel= "B737-3B7"
+      this.fromEngineModel= "CFM56-3B1"
+     },
+      melbBenMethod() {
+      this.fromCity = "Melbourne"
+      this.toCity= "Bendigo"
+      this.toCost= "70.00"
+      this.returnCost= "70.00"
+      this.toPlaneModel= "A320-232"
+      this.toEngineModel= "Unknown"
+      this.fromPlaneModel= "A320-232"
+      this.fromEngineModel= "Unknown"
+     },
+      melbLaunMethod() {
+      this.fromCity = "Melbourne"
+      this.toCity= "Launceston"
+      this.toCost= "100.00"
+      this.returnCost= "100.00"
+      this.toPlaneModel= "B737-3B7"
+      this.toEngineModel= "CFM56-3B1"
+      this.fromPlaneModel= "B737-476"
+      this.fromEngineModel= "CFM-56-3"
+     },
+      adeBrokeMethod() {
+      this.fromCity = "Adelaide"
+      this.toCity= "Broken Hill"
+      this.toCost= "100.00"
+      this.returnCost= "100.00"
+      this.toPlaneModel= "A320-232"
+      this.toEngineModel= "V2527-5A"
+      this.fromPlaneModel= "A320-232"
+      this.fromEngineModel= "V2527-5A"
+     },
+      adePerthMethod() {
+      this.fromCity = "Adelaide"
+      this.toCity= "Perth"
+      this.toCost= "220.00"
+      this.returnCost= "220.00"
+      this.toPlaneModel= "A330-203"
+      this.toEngineModel= "CF6-80E142"
+      this.fromPlaneModel= "A330-203"
+      this.fromEngineModel= "CF6-80E142"
+     },
+      adeDarMethod() {
+      this.fromCity = "Adelaide"
+      this.toCity= "Darwin"
+      this.toCost= "230.00"
+      this.returnCost= "230.00"
+      this.toPlaneModel= "A330-203"
+      this.toEngineModel= "CF6-80E142"
+      this.fromPlaneModel= "A330-203"
+      this.fromEngineModel= "CF6-80E142"
+     },
+      darAliceMethod() {
+      this.fromCity = "Darwin"
+      this.toCity= "Alice Spring"
+      this.toCost= "120.00"
+      this.returnCost= "120.00"
+      this.toPlaneModel= "B737-467"
+      this.toEngineModel= "CFM-56-3"
+      this.fromPlaneModel= "B737-476"
+      this.fromEngineModel= "CFM-56-3"
+     },
+      perthAlbMethod() {
+      this.fromCity = "Perth"
+      this.toCity= "Albany"
+      this.toCost= "100.00"
+      this.returnCost= "100.00"
+      this.toPlaneModel= "A320-232"
+      this.toEngineModel= "V2527-5A"
+      this.fromPlaneModel= "A320-232"
+      this.fromEngineModel= "V2527-5A"
+     },
+      perthKalMethod() {
+      this.fromCity = "Perth"
+      this.toCity= "Kalgoorlie"
+      this.toCost= "80.00"
+      this.returnCost= "80.00"
+      this.toPlaneModel= "A320-232"
+      this.toEngineModel= "V2527-5A"
+      this.fromPlaneModel= "A320-232"
+      this.fromEngineModel= "V2527-5A"
+     },
+      perthBroMethod() {
+      this.fromCity = "Perth"
+      this.toCity= "Broome"
+      this.toCost= "90.00"
+      this.returnCost= "90.00"
+      this.toPlaneModel= "A320-232"
+      this.toEngineModel= "V2527-5A"
+      this.fromPlaneModel= "B737-476"
+      this.fromEngineModel= "CFM-56-3"
+     },
+      launHobMethod() {
+      this.fromCity = "Launceston"
+      this.toCity= "Hobart"
+      this.toCost= "80.00"
+      this.returnCost= "80.00"
+      this.toPlaneModel= "A320-232"
+      this.toEngineModel= "V2527-5A"
+      this.fromPlaneModel= "A320-232"
+      this.fromEngineModel= "V2527-5A"
+     },
+      brisMtMethod() {
+      this.fromCity = "Brisbane"
+      this.toCity= "Mt Isa"
+      this.toCost= "170.00"
+      this.returnCost= "170.00"
+      this.toPlaneModel= "B737-3B7"
+      this.toEngineModel= "CFM56-3B1"
+      this.fromPlaneModel= "A330-202"
+      this.fromEngineModel= "CF6-80E142"
+     },
+      brisRockMethod() {
+      this.fromCity = "Brisbane"
+      this.toCity= "Rockhampton"
+      this.toCost= "180.00"
+      this.returnCost= "180.00"
+      this.toPlaneModel= "B737-3B7"
+      this.toEngineModel= "CFM56-3B1"
+      this.fromPlaneModel= "A330-202"
+      this.fromEngineModel= "CF6-80E142"
+     },
+      brisCairMethod() {
+      this.fromCity = "Brisbane"
+      this.toCity= "Cairns"
+      this.toCost= "230.00"
+      this.returnCost= "230.00"
+      this.toPlaneModel= "A330-203"
+      this.toEngineModel= "CF6-80E142"
+      this.fromPlaneModel= "A330-203"
+      this.fromEngineModel= "CF6-80E142"
+     },
+      brisDarMethod() {
+      this.fromCity = "Brisbane"
+      this.toCity= "Darwin"
+      this.toCost= "240.00"
+      this.returnCost= "240.00"
+      this.toPlaneModel= "A330-203"
+      this.toEngineModel= "CF6-80E142"
+      this.fromPlaneModel= "A330-203"
+      this.fromEngineModel= "CF6-80E142"
+     },
+      mtDarMethod() {
+      this.fromCity = "Mt Isa"
+      this.toCity= "Darwin"
+      this.toCost= "120.00"
+      this.returnCost= "120.00"
+      this.toPlaneModel= "B737-3B7"
+      this.toEngineModel= "CFM56-3B1"
+      this.fromPlaneModel= "B737-3B7"
+      this.fromEngineModel= "CFM56-3B1"
+     },
+      adePtMethod() {
+      this.fromCity = "Adelaide"
+      this.toCity= "Pt Augusta"
+      this.toCost= "50.00"
+      this.returnCost= "50.00"
+      this.toPlaneModel= "B717-200"
+      this.toEngineModel= "Unknown"
+      this.fromPlaneModel= "B717-200"
+      this.fromEngineModel= "Unknown"
      }
   }
 }
